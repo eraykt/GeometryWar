7af220b8755bbdb41cd0484e98970d7df7066e75
eray <alieraykt@gmail.com>
2025-11-06 21:29:31 +0300

    Vec2 implemented

--- Vec2.cpp
+++ Vec2.cpp
@@ -1,62 +1,81 @@
 #include "Vec2.h"
 #include <cmath>
 
-
 Vec2::Vec2() = default;
 
 Vec2::Vec2(float xin, float yin) : x(xin), y(yin) {}
 
-bool Vec2::operator==(const Vec2 &rhs) const {
-    return x == rhs.x && y == rhs.y;
+bool Vec2::operator==(const Vec2& rhs) const
+{
+	return x == rhs.x && y == rhs.y;
 }
 
-bool Vec2::operator!=(const Vec2 &rhs) const {
-    return x != rhs.x || y != rhs.y;
+bool Vec2::operator!=(const Vec2& rhs) const
+{
+	return x != rhs.x || y != rhs.y;
 }
 
-Vec2 Vec2::operator+(const Vec2 &rhs) const {
-    return {x + rhs.x, y + rhs.y};
+Vec2 Vec2::operator+(const Vec2& rhs) const
+{
+	return { x + rhs.x, y + rhs.y };
 }
 
-Vec2 Vec2::operator-(const Vec2 &rhs) const {
-    return {x - rhs.x, y - rhs.y};
+Vec2 Vec2::operator-(const Vec2& rhs) const
+{
+	return { x - rhs.x, y - rhs.y };
 }
 
-Vec2 Vec2::operator*(const float val) const {
-    return {x * val, y * val};
+Vec2 Vec2::operator*(const float val) const
+{
+	return { x * val, y * val };
 }
 
-Vec2 Vec2::operator/(const float val) const {
-    return {x / val, y / val};
+Vec2 Vec2::operator/(const float val) const
+{
+	return { x / val, y / val };
 }
 
-Vec2 Vec2::operator+=(const Vec2 &rhs) const {
-    // TODO
-    return {0.0f, 0.0f};
+Vec2& Vec2::operator+=(const Vec2& rhs)
+{
+	x += rhs.x;
+	y += rhs.y;
+
+	return *this;
 }
 
-Vec2 Vec2::operator-=(const Vec2 &rhs) const {
-    // TODO
-    return {0.0f, 0.0f};
+Vec2& Vec2::operator-=(const Vec2& rhs)
+{
+	x -= rhs.x;
+	y -= rhs.y;
+
+	return *this;
 }
 
-Vec2 Vec2::operator/=(const float val) const {
-    // TODO
-    return {0.0f, 0.0f};
+Vec2& Vec2::operator/=(const float val)
+{
+	x /= val;
+	y /= val;
+
+	return *this;
 }
 
-Vec2 Vec2::operator*=(const float val) const {
-    // TODO
-    return {0.0f, 0.0f};
+Vec2& Vec2::operator*=(const float val)
+{
+	x *= val;
+	y *= val;
+
+	return *this;
 }
 
-float Vec2::dist(const Vec2 &rhs) const {
-    // float dx = rhs.x - x;
-    // float dy = rhs.y - y;
-    // return std::sqrt(dx * dx + dy * dy);
-    return (rhs - *this).length();
+float Vec2::dist(const Vec2& rhs) const
+{
+	// float dx = rhs.x - x;
+	// float dy = rhs.y - y;
+	// return std::sqrt(dx * dx + dy * dy);
+	return (rhs - *this).length();
 }
 
-float Vec2::length() const {
-    return std::sqrt(x * x + y * y);
+float Vec2::length() const
+{
+	return std::sqrt(x * x + y * y);
 }
--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@
 
 // Use GPU, not integrated.
 extern "C" {
-    __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+	__declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
 }
 extern "C" {
-    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
 }
 
-int main() {
-    Game g("config.txt");
-    g.run();
+int main()
+{
+	Game g("config.txt");
+	g.run();
 
-    return 0;
+	return 0;
 }
- 
